55804bd0f2a08e6a9bb2a85347aeb5577ba8db86
PO-YU CHEN <slimon0216@gmail.com>
2024-10-11 16:54:30 +0800

    assign1: finish

--- assign1/imdb.cc
+++ assign1/imdb.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -9,39 +10,141 @@ using namespace std;
 const char *const imdb::kActorFileName = "actordata";
 const char *const imdb::kMovieFileName = "moviedata";
 imdb::imdb(const string& directory) {
-  const string actorFileName = directory + "/" + kActorFileName;
-  const string movieFileName = directory + "/" + kMovieFileName;  
-  actorFile = acquireFileMap(actorFileName, actorInfo);
-  movieFile = acquireFileMap(movieFileName, movieInfo);
+    const string actorFileName = directory + "/" + kActorFileName;
+    const string movieFileName = directory + "/" + kMovieFileName;  
+    actorFile = acquireFileMap(actorFileName, actorInfo);
+    movieFile = acquireFileMap(movieFileName, movieInfo);
 }
 
 bool imdb::good() const {
-  return !( (actorInfo.fd == -1) || 
-	    (movieInfo.fd == -1) ); 
+    return !( (actorInfo.fd == -1) || 
+            (movieInfo.fd == -1) ); 
 }
 
 imdb::~imdb() {
-  releaseFileMap(actorInfo);
-  releaseFileMap(movieInfo);
+    releaseFileMap(actorInfo);
+    releaseFileMap(movieInfo);
 }
 
 bool imdb::getCredits(const string& player, vector<film>& films) const { 
-  return false; 
+    int numPlayers = *(int*)actorFile;
+    // cout << numPlayers << endl;
+    //for (int i = 0; i < numPlayers; ++i) {
+    //	int offset = *((int*)actorFile + 1 + i); 
+    //	string name = (char*)actorFile + offset ;
+    //	cout << name  << '\n';
+    //}
+
+    // Find player with binary search;
+    // Create a pointer to the beginning of actor offsets
+    int* offsetsBegin = (int*)actorFile + 1;
+    int* offsetsEnd = offsetsBegin + numPlayers;
+
+    // Use std::lower_bound to find the player
+    int* result = std::lower_bound(offsetsBegin, offsetsEnd, player, [&](int offset, const std::string& key) {
+            std::string name = (char*)actorFile + offset;
+            return name < key;
+            });
+
+    if (result == offsetsEnd) {
+        return false; // Player not found
+    }
+
+    int offset = *result;
+    char* actor = (char*)actorFile + offset;
+    std::string name = actor;
+    if (name != player) return false; // Ensure the name matches exactly
+
+
+
+    // cout << "Find " << name << endl;
+
+    // Find credits
+    char* ptr = actor + name.size()+1;
+    if (!(name.size() &1)) ptr++;
+    // next 2 bytes (short) represent # of movies
+    short numFilms = *(short*)ptr;
+    // cout << name << " has " << numFilms << '\n';
+
+    /* 
+       If the number of bytes dedicated to the actor’s name (always even) and the short (always 2)
+       isn’t a multiple of four, then two additional '\0'’s appear after the two bytes storing the
+       number of movies.
+       */
+    ptr += 2;
+    while ((ptr-actor)%4 != 0) ptr++;
+    for (int i = 0; i < numFilms; ++i) {
+        int movieOffset = *((int*)ptr + i);
+        char *ptr = (char*)movieFile + movieOffset;
+        string filmName = ptr;
+        ptr += filmName.size()+1;
+        int year = *ptr + 1900;
+        // cout << "Film: " << filmName << ' ' << year << "\n";
+        film f;
+        f.title = std::move(filmName);
+        f.year = year;
+        films.push_back(std::move(f));
+    }
+
+    return true;
 }
 
 bool imdb::getCast(const film& movie, vector<string>& players) const { 
-  return false; 
+    int numMovies = *(int*)movieFile;	
+    //
+    // Use std::lower_bound to find the movie
+    int* offsetsBegin = (int*)movieFile + 1;
+    int* offsetsEnd = (int*)movieFile + numMovies;
+    int* result = std::lower_bound(offsetsBegin, offsetsEnd, movie, [&] (int offset, const film& key) {
+            char* ptr = (char*)movieFile + offset;
+            film cur;
+            cur.title = ptr;
+            ptr += cur.title.size()+1;
+            cur.year = *ptr + 1900;
+            return cur < key;
+            });
+    if (result == offsetsEnd) 
+        return false; 
+    int offset = *result;
+    char* ptr = (char*)movieFile + offset;
+    char* movieStart = ptr;
+    film movieFound;
+    movieFound.title = ptr;	
+    ptr += movieFound.title.size()+1;
+    movieFound.year = *ptr + 1900;
+    if (!(movieFound == movie)) return false;
+    // cout << movieFound.title << ' ' << movieFound.year << endl;
+    // find players
+    // If the total number of bytes used to encode the name and year of the movie is odd, then an extra '\0' sits in between the one-byte year and the data that follows.
+    char *movie_ptr = (char *) movieFile + offset;
+    long titleSpaceUsed = movie.title.length() + 1;
+    movie_ptr += titleSpaceUsed;
+
+    // Skip year
+    long yearSpaceUsed = ((titleSpaceUsed + 1) % 2 == 0) ? 1 : 2;
+    movie_ptr += yearSpaceUsed;
+
+    // Skip number of actors
+    short numActors = *(short *) movie_ptr;
+    movie_ptr += ((titleSpaceUsed + yearSpaceUsed + 2) % 4 == 0) ? 2 : 4;
+
+    // Store all players into vector
+    for (short i=0; i<numActors; i++) {
+        players.push_back((char*)actorFile + (*(int *) movie_ptr));
+        movie_ptr += 4;
+    }
+    return true;
 }
 
 const void *imdb::acquireFileMap(const string& fileName, struct fileInfo& info) {
-  struct stat stats;
-  stat(fileName.c_str(), &stats);
-  info.fileSize = stats.st_size;
-  info.fd = open(fileName.c_str(), O_RDONLY);
-  return info.fileMap = mmap(0, info.fileSize, PROT_READ, MAP_SHARED, info.fd, 0);
+    struct stat stats;
+    stat(fileName.c_str(), &stats);
+    info.fileSize = stats.st_size;
+    info.fd = open(fileName.c_str(), O_RDONLY);
+    return info.fileMap = mmap(0, info.fileSize, PROT_READ, MAP_SHARED, info.fd, 0);
 }
 
 void imdb::releaseFileMap(struct fileInfo& info) {
-  if (info.fileMap != NULL) munmap((char *) info.fileMap, info.fileSize);
-  if (info.fd != -1) close(info.fd);
+    if (info.fileMap != NULL) munmap((char *) info.fileMap, info.fileSize);
+    if (info.fd != -1) close(info.fd);
 }
--- assign1/search.cc
+++ assign1/search.cc
@@ -1,6 +1,66 @@
+#include <algorithm>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include "imdb.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  return 0;
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <actor> " << "<actor>" << endl;
+        return 1;
+    }
+    imdb db(kIMDBDataDirectory);
+    if (!db.good()) {
+        cerr << "Data file not found!  Exiting..." << endl;
+        return 1;
+    }
+    // do the bfs to find the relation between the two actors
+    string source = argv[1];
+    string target = argv[2];
+    std::queue<string> q;
+    std::map<string, string> parent; // can be player or film
+    std::set<string> visited;
+    parent[source] = "";
+    visited.insert(source);
+    q.push(source);
+    while (q.size()) {
+        string cur = q.front();
+        q.pop();
+        if (cur == target) {
+            vector<string> path;
+            while (cur != "") {
+                path.push_back(cur);
+                cur = parent[cur];
+            }
+            reverse(path.begin(), path.end());
+            for (int i = 0; i+2 < path.size(); i+=2) {
+                cout << path[i] << " was in \"" << path[i+1] << "\" with " << path[i+2] << '.' << endl;
+            }
+            return 0;
+        }
+        vector<film> films;
+        if (!db.getCredits(cur, films))
+            continue;
+        for (const film &f : films) {
+            if (visited.count(f.title))
+                continue;
+            visited.insert(f.title);
+            parent[f.title] = cur;
+            vector<string> actors;
+            if (!db.getCast(f, actors))
+                continue;
+            for (string &a : actors) {
+                if (visited.count(a))
+                    continue;
+                parent[a] = f.title;
+                visited.insert((a));
+                q.push(std::move(a));
+            }
+        }
+    }
+    return 0;
 }
